1bff4690ebef1489a2f9f25e64afb5beee6a15f0
Mustafa Gaeed <38606881-mustafangaid@users.noreply.replit.com>
2025-01-16 08:16:23 +0000

    add Copyright Comments for all code parts

--- src/encryption.c
+++ src/encryption.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2025 Mustafa Naseer
+ *
+ * This file is part of UruCrypt encryption application.
+ *
+ * UruCrypt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * UruCrypt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UruCrypt. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "../include/encryption.h"
 #include "../include/error_handling.h"
 #include "../include/key_management.h"
